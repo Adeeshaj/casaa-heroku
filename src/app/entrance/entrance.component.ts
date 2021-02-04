5ca318a0962e8d635a153cb46ebe9acfce2bf2e6
adeeshaj <adeesha.14@cse.mrt.ac.lk>
2021-02-04 06:56:37 +0530

    add landing page dots and fixed some bugs

--- src/app/entrance/entrance.component.ts
+++ src/app/entrance/entrance.component.ts
@@ -9,6 +9,7 @@ import { SlickCarouselComponent } from 'ngx-slick-carousel';
 })
 export class EntranceComponent implements OnInit {
   CURRENT_SLIDE: number;
+  NEXT_SLIDE: number;
   TIMEOUT: number;
   StartTime: number;
   @ViewChild('slickModal') slickModal: SlickCarouselComponent;
@@ -31,86 +32,86 @@ export class EntranceComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.CURRENT_SLIDE = 1
+    this.CURRENT_SLIDE = 0
     this.TIMEOUT = 10
     this.StartTime = new Date().getTime()
   }
 
-  getUrl(slide){
-    if(this.isTimeElapsed(this.StartTime)){
-      if(slide == 'right' || slide == 'next'){
-        return this.getUrlById(this.right());
-      } else if (slide == 'left') {
-        return this.getUrlById(this.left());
-      }
-    } 
-  }
+  // getUrl(slide){
+  //   if(this.isTimeElapsed(this.StartTime)){
+  //     if(slide == 'right' || slide == 'next'){
+  //       return this.getUrlById(this.right());
+  //     } else if (slide == 'left') {
+  //       return this.getUrlById(this.left());
+  //     }
+  //   } 
+  // }
 
-  getUrlById(id){
-    if(id==1){
-      return "url('/assets/img/Landing Page/1 .jpg')";
-    } else if(id == 2) {
-      return "url('/assets/img/Landing Page/2 .mp4')";
-    } else if(id == 3) {
-      return "url('/assets/img/Landing Page/3 .jpg')";
-    } else if(id == 4) {
-      return "url('/assets/img/Landing Page/4 .jpg')";
-    } else if(id == 5) {
-      return "url('/assets/img/Landing Page/5 .jpg')";
-    } else if(id == 6) {
-      return "url('/assets/img/Landing Page/6 .mp4')";
-    } else {
-      return "url('/assets/img/Landing Page/1 .jpg')";
-    }
-  }
+  // getUrlById(id){
+  //   if(id==1){
+  //     return "url('/assets/img/Landing Page/1 .jpg')";
+  //   } else if(id == 2) {
+  //     return "url('/assets/img/Landing Page/2 .mp4')";
+  //   } else if(id == 3) {
+  //     return "url('/assets/img/Landing Page/3 .jpg')";
+  //   } else if(id == 4) {
+  //     return "url('/assets/img/Landing Page/4 .jpg')";
+  //   } else if(id == 5) {
+  //     return "url('/assets/img/Landing Page/5 .jpg')";
+  //   } else if(id == 6) {
+  //     return "url('/assets/img/Landing Page/6 .mp4')";
+  //   } else {
+  //     return "url('/assets/img/Landing Page/1 .jpg')";
+  //   }
+  // }
   
-  right(){
-    this.CURRENT_SLIDE +=1
-    if (this.CURRENT_SLIDE<=6){
-      console.log(this.getUrlById(this.CURRENT_SLIDE));
-    } else if (this.CURRENT_SLIDE>6){
-      this.CURRENT_SLIDE = 1
-      console.log(this.getUrlById(this.CURRENT_SLIDE));
-    }
-  }
+  // right(){
+  //   this.CURRENT_SLIDE +=1
+  //   if (this.CURRENT_SLIDE<=6){
+  //     console.log(this.getUrlById(this.CURRENT_SLIDE));
+  //   } else if (this.CURRENT_SLIDE>6){
+  //     this.CURRENT_SLIDE = 1
+  //     console.log(this.getUrlById(this.CURRENT_SLIDE));
+  //   }
+  // }
 
-  left(){
-    this.CURRENT_SLIDE -=1
-    if (this.CURRENT_SLIDE>=1){
-      console.log(this.getUrlById(this.CURRENT_SLIDE));
-    } else if (this.CURRENT_SLIDE<1){
-      this.CURRENT_SLIDE = 6
-      console.log(this.getUrlById(this.CURRENT_SLIDE));
-    }
-  }
+  // left(){
+  //   this.CURRENT_SLIDE -=1
+  //   if (this.CURRENT_SLIDE>=1){
+  //     console.log(this.getUrlById(this.CURRENT_SLIDE));
+  //   } else if (this.CURRENT_SLIDE<1){
+  //     this.CURRENT_SLIDE = 6
+  //     console.log(this.getUrlById(this.CURRENT_SLIDE));
+  //   }
+  // }
 
-  calcuateStylesLeft() {
-    return {
-      'background-image': this.getUrl('left'),
-      'background-size': 'cover',
-      'background-position': 'center',
-      'width': '1366px', 
-      'height':'768px', 
-      'position': 'absolute',
-      'left': '-1366px',
-      'top': '0px',
-      'z-index': 0
-    }
-  }
+  // calcuateStylesLeft() {
+  //   return {
+  //     'background-image': this.getUrl('left'),
+  //     'background-size': 'cover',
+  //     'background-position': 'center',
+  //     'width': '1366px', 
+  //     'height':'768px', 
+  //     'position': 'absolute',
+  //     'left': '-1366px',
+  //     'top': '0px',
+  //     'z-index': 0
+  //   }
+  // }
 
-  calcuateStylesRight(){
-    return {
-      'background-image': this.getUrl('right'),
-      'background-size': 'cover',
-      'background-position': 'center',
-      'width': '1366px', 
-      'height':'768px', 
-      'position': 'absolute',
-      'left': '0px',
-      'top': '0px',
-      'z-index': 1
-    }
-  }
+  // calcuateStylesRight(){
+  //   return {
+  //     'background-image': this.getUrl('right'),
+  //     'background-size': 'cover',
+  //     'background-position': 'center',
+  //     'width': '1366px', 
+  //     'height':'768px', 
+  //     'position': 'absolute',
+  //     'left': '0px',
+  //     'top': '0px',
+  //     'z-index': 1
+  //   }
+  // }
 
   isTimeElapsed(start){
     var elapsed = new Date().getTime() - start;
@@ -121,19 +122,45 @@ export class EntranceComponent implements OnInit {
     }
   }
 
+  getVisibility(component, component_id){
+    if(component=='line'){
+      if (component_id == this.CURRENT_SLIDE){
+        return 'block';
+      } else {
+        return 'none';
+      }
+    } else if(component=='dot'){
+      if (component_id == this.CURRENT_SLIDE){
+        return 'none';
+      } else {
+        return 'block';
+      }
+    } else if(component=='dot-selected'){
+      if (component_id == this.CURRENT_SLIDE){
+        return 'block';
+      } else {
+        return 'none';
+      }
+    }
+  }
+
   slickInit(e) {
     console.log('slick initialized');
   }
   
   breakpoint(e) {
+
     console.log('breakpoint');
   }
   
   afterChange(e) {
+    this.CURRENT_SLIDE = e.currentSlide
+    this.NEXT_SLIDE = e.nextSlide
     console.log('afterChange');
   }
   
   beforeChange(e) {
+
     console.log('beforeChange');
   }
 
@@ -143,5 +170,9 @@ export class EntranceComponent implements OnInit {
   
   prev() {
     this.slickModal.slickPrev();
+  }
+
+  goTo(slide){
+    this.slickModal.slickGoTo(slide);
   }
 }
